1ed94523aef69dcae99c2ad131ad5833f6288f94
david <chan15tw@gmail.com>
2013-08-18 12:23:45 +0800

    add include js and css file

--- admin/admin.php
+++ admin/admin.php
@@ -3,6 +3,9 @@ include'../main.php';
 include 'login-policy.php';
 $pageItemName = ''; // nav name
 $subItemName = ''; // sidebar name
+$incCssArr = array();
+$incJsArr = array();
+include 'inc.php';
 include 'nav.php';
 
 $smarty->display('admin/welcome.tpl');
